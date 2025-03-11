d1d0312befe8a8bc7ca92c3f33fac807ec7679f9
Ridwan <ridghamy@gmail.com>
2025-03-11 13:11:55 +0400

    Added feature to delete particular student by email and display all students from database

--- MainProject/MainProject/StudentOperations.cs
+++ MainProject/MainProject/StudentOperations.cs
@@ -1,14 +1,11 @@
-﻿using System.Text.RegularExpressions;
-using System.Globalization;
-using MainProject.Context;
+﻿using MainProject.Context;
 using MainProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MainProject;
 public class StudentOperations
 {
     // CRUD Operations for students
-
-    // Student
     public void AddStudent()
     {
         string? firstName;
@@ -25,7 +22,7 @@ public class StudentOperations
         {
             Console.Write("First Name: ");
             firstName = Console.ReadLine() ?? "";
-            if (ValidateString(firstName)) break;
+            if (Validations.ValidateString(firstName)) break;
             Console.WriteLine("First name can't be empty, please re-input.");
         }
 
@@ -33,7 +30,7 @@ public class StudentOperations
         {
             Console.Write("Last Name: ");
             lastName = Console.ReadLine() ?? "";
-            if (ValidateString(lastName)) break;
+            if (Validations.ValidateString(lastName)) break;
             Console.WriteLine("Last name can't be empty, please re-input.");
         }
 
@@ -41,63 +38,90 @@ public class StudentOperations
         {
             Console.Write("Email: ");
             email = Console.ReadLine() ?? "";
-            if (ValidateString(email))
+            if (Validations.ValidateString(email))
             {
-                // TODO: Check if email is already present in database; email should be unique.
+                
                 // Validate the email
-                if (ValidateEmail(email)) break;
+                if (Validations.ValidateEmail(email))
+                {
+                    // Check if email is already present in database; emails should be unique
+                    if (!Validations.CheckEmailExistence(email))
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine("Email already exists, please choose another one.");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong email format; format should be in the form John.Doe@example.com, please re-input.");
+                }
             }
-            Console.WriteLine("Email can't be empty, please re-input.");
+            else
+            {
+                Console.WriteLine("Email can't be empty, please re-input.");
+            }
         }
 
         while (true)
         {
             Console.Write("Password: ");
             password = Console.ReadLine() ?? "";
-            if (ValidateString(password))
+            if (Validations.ValidateString(password))
             {
-                if (ValidatePassword(password))
+                if (Validations.ValidatePassword(password))
                 {
                     // TODO: Hash password then store in database.
+                    // TODO: Double entry-check for password.
                     break;
                 }
             }
-            Console.WriteLine("Password can't be empty, please re-input.");
+            else
+            {
+                Console.WriteLine("Password can't be empty, please re-input.");
+            }
         }
 
         while (true)
         {
-            Console.Write("Date of birth (dd/MM/yyyy): ");
+            Console.Write("Date of birth (yyyy/MM/dd): ");
             var date = Console.ReadLine() ?? "";
-            if (ValidateString(date))
+            if (Validations.ValidateString(date))
             {
-                if (ValidateDate(date))
+                if (Validations.ValidateDate(date))
                 {
                     DateOnly.TryParse(date, out dateOfBirth);
                     break;
                 }
                 Console.WriteLine("Incorrect date format, please use the format yyyy/MM/dd.");
             }
-            Console.WriteLine("Date can't be empty, please re-input.");
+            else
+            {
+                Console.WriteLine("Date can't be empty, please re-input.");
+            }
+            
         }
 
         while (true)
         {
             Console.Write("Phone Number: ");
             phoneNumber = Console.ReadLine() ?? "";
-            if (ValidateString(phoneNumber))
+            if (Validations.ValidateString(phoneNumber))
             {
-                if (ValidatePhoneNumber(phoneNumber)) break;
+                if (Validations.ValidatePhoneNumber(phoneNumber)) break;
                 Console.WriteLine("Incorrect phone number format, phone number should be in the format (+230)58226843 where the number in parentheses is optional.");
             }
-            Console.WriteLine("Phone number can't be empty, please re-input.");
+            else
+            {
+                Console.WriteLine("Phone number can't be empty, please re-input.");
+            }
         }
 
         while (true)
         {
             Console.Write("Address: ");
             address = Console.ReadLine() ?? "";
-            if (ValidateString(address)) break;
+            if (Validations.ValidateString(address)) break;
             Console.WriteLine("Address can't be empty, please re-input.");
         }
 
@@ -119,6 +143,7 @@ public class StudentOperations
                 };
                 context.Students.Add(student);
                 context.SaveChanges();
+                Console.WriteLine("Student added successfully.");
             }
         }
         catch (Exception e)
@@ -127,48 +152,65 @@ public class StudentOperations
         }
     }
 
-    bool ValidateString(string text) => !string.IsNullOrEmpty(text);
-
-    bool ValidateEmail(string email)
+    public void DeleteStudent()
     {
-        var emailRegex = new Regex(@"\w+@[a-zA-Z\.]+\.\w+");
-        return emailRegex.IsMatch(email);
+        // Delete by email
+        string? email;
+        while (true)
+        {
+            Console.Write("Please enter the email of the student you want to delete: ");
+            email = Console.ReadLine();
+            if (Validations.ValidateString(email))
+            {
+                if (Validations.ValidateEmail(email))
+                {
+                    //TODO: Check if email is present in database
+                    if (Validations.CheckEmailExistence(email))
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong email format; format should be in the form John.Doe@example.com, please re-input.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Email can't be empty, please re-input.");
+            }
+        }
+        try
+        {
+            using (var context = new DrivingLessonBookingSystemContext())
+            {
+                context.Students.Where(s => s.Email == email).ExecuteDelete();
+                Console.WriteLine("Student deleted successfully.");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Processing failed: {e.Message}");
+        }
     }
 
-    bool ValidatePassword(string password)
+    public void DisplayStudent()
     {
-        var passwordRegex = new Regex(@"(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&*\(\)\-\!+\?])[A-Za-z\d@#$%^&*\(\)\-\!+\?]{12,}");
-        const string uppercasePattern = "(.*[A-Z])";
-        const string lowercasePattern = "(.*[a-z])";
-        const string digitPattern = @"(.*\d)";
-        const string symbolPattern = @"(.*[@#$%^&*\(\)\-\!+\?])";
-        const string lengthPattern = @"([A-Za-z\d@#$%^&*\(\)\-\!+\?]{12,})";
-
-        var containUpperCase = Regex.IsMatch(password, uppercasePattern);
-        var containLowerCase = Regex.IsMatch(password, lowercasePattern);
-        var containDigit = Regex.IsMatch(password, digitPattern);
-        var containSymbol = Regex.IsMatch(password, symbolPattern);
-        var validLength = Regex.IsMatch(password, lengthPattern);
-
-        if (!containUpperCase) Console.WriteLine("Password should contain at least one uppercase letter.");
-        if (!containLowerCase) Console.WriteLine("Password should contain at least one lowercase letter.");
-        if (!containDigit) Console.WriteLine("Password should contain at least one digit.");
-        if (!containSymbol) Console.WriteLine("Password should contain at least one special character.");
-        if (!validLength) Console.WriteLine("Password is too short, it should be at least 12 characters long.");
-
-        return passwordRegex.IsMatch(password);
-    }
-
-    bool ValidateDate(string date)
-    {
-        var format = "yyyy/MM/dd";
-        return DateOnly.TryParseExact(date, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
-    }
-
-    bool ValidatePhoneNumber(string phoneNumber)
-    {
-        var phoneNumberRegex = new Regex(@"(\(\+\d+\))?((\s+)?\d{8})");
-        return phoneNumberRegex.IsMatch(phoneNumber.Trim());
+        try
+        {
+            using (var context = new DrivingLessonBookingSystemContext())
+            {
+                var students = context.Students.AsNoTracking().ToList();
+                foreach (var student in students)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Processing failed: {e.Message}");
+        }
     }
 }
 
