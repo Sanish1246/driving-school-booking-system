4cf37a530b0ddf5aac2c240c7c2c0045ad7135af
Julien Doolub <doolubjulien@gmail.com>
2025-04-14 14:03:42 +0400

    Setting up new TestProject
    -added virtual to db context properties.
    - created new empty constructor and another one for testing in db context
    -made some changes in onconfig method in db for inmemory db usage for testing
    -set up the requirements for testing student operations

---
+++ MainProject/TestProject/TestStudent.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using MainProject.Context;
+using MainProject.Models;
+using MainProject;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace TestProject
+{
+    [DoNotParallelize]
+    [TestClass]
+    public class TestStudent
+    {
+        private const string InMemoryDbName = "SharedInMemoryDB";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<DrivingLessonBookingSystemContext>()
+                .UseInMemoryDatabase(databaseName: InMemoryDbName)
+                .Options;
+
+            using (var context = new DrivingLessonBookingSystemContext(options))
+            {
+                context.Students.Add(new Student
+                {
+                    FirstName = "zouzou", LastName = "mounou", Email = "zoumounou@gmail.com",
+                    DateOfBirth = new DateOnly(2003, 04, 18), Address = "2 lalaland Main St",
+                    PhoneNumber = "+23058226843", Password = "ZouzouMounou123!",
+                });
+
+                context.Students.Add(new Student
+                {
+                    FirstName = "Janette", LastName = "Ginger", Email = "janettesginger@gmail.com",
+                    DateOfBirth = new DateOnly(2014, 12, 2), Address = "soobajee lane",
+                    PhoneNumber = "+23054536844", Password = "tihisWrtr456!" 
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
